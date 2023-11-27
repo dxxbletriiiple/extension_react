c199338ace785572313003bbb6eff7ba61f147db
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-11-27 05:47:32 +0300

    edit: first rendder

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,27 @@
+import { Button } from '../Button';
+import { InputWrapper } from '../InputWrapper';
 import st from './App.module.scss';
 
 export const App = () => {
+	const onAdd = () => {};
+	const onSave = () => {};
+
 	return (
 		<div className={st.app}>
 			<div className={st.title}>Add Experiments</div>
+			<div className={st.wrapper}>
+				<InputWrapper
+					index={1}
+					onChange={() => {}}
+					onDelete={() => {
+						console.log(222);
+					}}
+				/>
+			</div>
+			<div className={st.btn_wrapper}>
+				<Button title='Add' onClick={onAdd} />
+				<Button title='Save' onClick={onSave} />
+			</div>
 		</div>
 	);
 };
